475b42fa5ecc23e0dd034f86a47e750c9b407e78
MajorXaker <alxkoleda@gmail.com>
2022-04-19 23:35:07 +0300

    sqlite restored for deploy

--- HuntLototron/settings.py
+++ HuntLototron/settings.py
@@ -92,19 +92,19 @@ WSGI_APPLICATION = 'HuntLototron.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
-
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'djangoadmins',
-        'PASSWORD': 'de1213fe',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'postgres',
+    #     'USER': 'djangoadmins',
+    #     'PASSWORD': 'de1213fe',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
     # heroku
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
