633090d2cb7112d06fa0ee9d3145055f7e67862f
MajorXaker <alxkoleda@gmail.com>
2022-04-20 00:33:24 +0300

    fix 20.04

--- HuntLototron/settings.py
+++ HuntLototron/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/files')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
